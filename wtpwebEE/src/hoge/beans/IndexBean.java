711df9a9e3a1e7164d8c00194e43be66dd722a69
kesakamo <kesakamo@KESAKAMO-JP.jp.oracle.com>
2018-06-02 09:01:34 +0900

    最初のコミット

---
+++ wtpwebEE/src/hoge/beans/IndexBean.java
@@ -0,0 +1,14 @@
+package hoge.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+@Named
+@RequestScoped
+public class IndexBean {
+	public String date(){
+		return (new java.util.Date()).toString();
+	}
+
+}
